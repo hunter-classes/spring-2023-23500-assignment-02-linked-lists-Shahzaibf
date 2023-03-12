222b0443948005ae7449511a9abc2ac05454c344
Shahzaib Fareed <shahzaibfareed1118@gmail.com>
2023-03-12 16:40:07 -0400

    Added class code

--- List.h
+++ List.h
@@ -7,29 +7,19 @@ class List{
   Node *head;
  public:
   List();
+  ~List();
   void insert(std::string data);
   void insert(int loc, std::string data); //0 indexed
   int length();
-  
+  std::string find(std::string data);
 
   std::string toString(); // for testing purposes
-    bool contains(std::string item);
-    void remove(int loc);
-  /*
-    Try to complete for Monday:
-    
-    get()
-    and think about the remove and 
-    other insert routine
-    
-    -------------------
-    ~List();
-    
   void remove(int loc);
+  bool contains(std::string item);
+  /*
+    
 
-  std::string get(int loc);
-  bool contains();
-  int length(); // the # of elements
+    
 
   */
   
